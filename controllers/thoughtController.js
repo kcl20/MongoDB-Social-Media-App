be9eb14eabfbb56031164c7cd67182f5e8d6ec95
Kenny Li <112835000+kcl20@users.noreply.github.com>
2023-03-09 00:17:23 -0500

    Add Thought and update User route working.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,41 +1,45 @@
 const { ObjectId } = require('mongoose').Types;
-const { User } = require('../models');
+const { Thought, User } = require('../models');
 
 
 module.exports = {
- // add thought
- addThought(req, res) {
+    // add thought
+    addThought(req, res) {
+        Thought.create(req.body)
+        .then(function (thought) {
+            return User.findOneAndUpdate(
+                { _id: req.body.username },
+                { $push: { thoughts: thought._id } },
+                { new: true }
+            );
+        })  
+        .then(function (user) {
+            res.json(user);
+        })
+        .catch((err) => res.status(500).json(err));
+    },
 
+    // updateThought(req, res) {
+    //     Thought.findOneAndUpdate()
+    // },
 
+    // remoteThought(req, res) {
+    //     Thought.findOneAndDelete()
+    // },
 
+    // getThought(req, res) {
+    //     Thought.findOne({ _id: req.params.thoughtId })
+    //     .then(function (thought) {
+    //         res.json(thought);
+    //     })
+    //     .catch((err) => res.status(500).json(err));
+    // },
 
-    .then(function (user) {
-        res.json(user);
-    })
-    .catch((err) => res.status(500).json(err));
-},
-
-updateThought(req, res) {
-    Thought.findOneAndUpdate()
-},
-
-remoteThought(req, res) {
-    Thought.findOneAndDelete()
-},
-
-getThought(req, res) {
-    Thought.findOne({ _id: req.params.thoughtId })
-    .then(function (thought) {
-        res.json(thought);
-    })
-    .catch((err) => res.status(500).json(err));
-},
-
-getAllThoughts(req, res) {
-    Thought.find({ _id: req.params.userId })
-    .then(function (thought) {
-        res.json(thought);
-    })
-    .catch((err) => res.status(500).json(err));
-}
+    // getAllThoughts(req, res) {
+    //     Thought.find({ _id: req.params.userId })
+    //     .then(function (thought) {
+    //         res.json(thought);
+    //     })
+    //     .catch((err) => res.status(500).json(err));
+    // }
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,24 +2,26 @@ const router = require('express').Router();
 
 const {
     addThought,
-    updateThought,
-    removeThought,
-    getThought,
-    getAllThoughts
+    // updateThought,
+    // removeThought,
+    // getThought,
+    // getAllThoughts,
+    // getAllThoughtsByUser
 } = require('../../controllers/thoughtController');
 
 
-// /api/thoughts/:userId/ route to post a new thought for a user
-router.route('/:userId/thoughts').post(addThought);
+// /api/thoughts/:userId/ route to post a new thought or get all thoughts
+router.route('/').post(addThought);
+// router.route('/').get(getAllThoughts).post(addThought);
 
 
 // /api/thoughts/:thoughtId route to delete a thought
-router.route('/:thoughtId').put(updateThought).delete(removeThought);
+// router.route('/:thoughtId').put(updateThought).delete(removeThought);
 
 // /api/thoughts/:thoughtId route to get a single thought
-router.route('/:thoughtId').get(getThought);
+// router.route('/:thoughtId').get(getThought);
 
 // /api/thoughts/:userId route to get all thoughts for a user
-router.route('/:userId').get(getAllThoughts);
+// router.route('/:userId').get(getAllThoughtsByUser);
 
 module.exports = router;
